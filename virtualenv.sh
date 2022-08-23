68589df9d89c10e0a42d65d3eb27b6223e715f88
gyume2021 <letsencrypthuang@gmail.com>
2022-08-23 13:15:17 +0800

    add cpg repo

---
+++ virtualenv.sh
@@ -0,0 +1,6 @@
+virtualenv -p /usr/bin/python3 cpg
+source cpg/bin/activate
+deactivate
+
+pip3 install torch torchvision torchaudio --extra-index-url https://download.pytorch.org/whl/cpu
+pip install tqdm
